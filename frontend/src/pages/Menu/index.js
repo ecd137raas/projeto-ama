a57ec29f5efd90e26420273c266eb0501021ed32
Emerson Duarte <raasjava@gmail.com>
2020-04-01 15:43:25 -0300

    Revert "Revert "Teste com material""
    This reverts commit 2a3d3fa990be54d56a2ea9679331ad7af07ab334.

---
+++ frontend/src/pages/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+export default function Menu() {
+  return (
+    <Button variant="contained" color="primary">
+      Olá Mundo
+    </Button>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Login from './pages/Login';
+import Signin from './pages/Signin';
 import Home from './pages/Home';
 import Employee from './pages/Employee';
+import Menu from './pages/Menu';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login} />
+                <Route path="/" exact component={Signin} />
                 <Route path="/home" component={Home} />
                 <Route path="/employee" component={Employee} />
+                <Route path="/menu" component={Menu} />
             </Switch>
         </BrowserRouter>
     )
